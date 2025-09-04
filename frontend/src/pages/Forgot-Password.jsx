c7c779c3e227c3f43ecc7a9e6bdb99bca51daa1c
Krish1212 <menikabaranwal121@gmail.com>
2025-09-04 23:31:13 +0530

    making

--- frontend/src/pages/Forgot-Password.jsx
+++ frontend/src/pages/Forgot-Password.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { IoIosArrowRoundBack } from "react-icons/io";
 function ForgotPassword() {
+    const [step,setStep] = useState(1);
+
     return (
-        <div>
-            <h2>Forgot Password</h2>
-            <p>Please enter your email address to reset your password.</p>
-            <input type="email" placeholder="Email" />
-            <button>Submit</button>
-        </div>
+        <div className="flex w-full items-center justify-center min-h-screen p-4 bg-[#fff9f6] ">
+            <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-8">
+                <div className="flex items-center gap-4 ">
+                    <IoIosArrowRoundBack size={30} className="text-[#ff6b6b]" />
+
+                    <h1 className="text-3xl font-bold text-center text-[#ff6b6b]">Forgot Password</h1>
+                </div>
+            </div>
+        </div>  
     );
 }
 
