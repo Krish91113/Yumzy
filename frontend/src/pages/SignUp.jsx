fb4eaff60fd6844690ea42515e38cd69f8b7277d
Krish1212 <krishbaranwal56@gmail.com>
2025-09-04 15:46:07 +0530

    K

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -7,7 +7,7 @@ function SignUp(){
     const bgColor = "#fff9f6";
     const borderColor = "#ddd";
     const [showPassword, setShowPassword] = useState(false);
-
+    const [role, setRole] = useState("user");
     return (
         <div className="min-h-screen w-full flex items-center justify-center p-4" style={{ backgroundColor: bgColor }}>
             <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-[1px] `} style={{
@@ -42,6 +42,28 @@ function SignUp(){
                     <input type={`${showPassword?"text":"password"}`} id="Password" className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:border-orange-500" placeholder="Enter your Password" style={{border : `1px solid ${borderColor}`}} />
                     <button className="absolute top-[12px] right-3 cursor-pointer text-gray-900" onClick={() => setShowPassword(!showPassword)}>{!showPassword?<IoEye />:<IoEyeOffOutline />}</button>
                     </div>
+                </div>
+
+                {/* {{RRoles}} */}
+
+                <div className="mb-4"> 
+                    <label htmlFor="Roles" className="block text-gray-700 font-medium mb-1">Roles   </label>
+                    <div className="flex gap-4">
+                        {["user","owner","deliveryboy"].map((r)=>(
+                            <button className="flex-1 border rounded-lg px-3 py-2 text-center font-medium cursor-pointer transition-colors"
+                            onClick={()=>setRole(r)}
+                            style={
+                                role==r?
+                                { backgroundColor: primaryColor, color: 'white' } : {border:`1px solid ${primaryColor}`, color: primaryColor}
+                            }
+                            >{r}</button>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <button className="w-full mt-4 bg-orange-500 text-white cursor-pointer font-medium py-2 px-4 rounded-lg hover:bg-orange-600 transition-colors">
+                        Sign up
+                    </button>
                 </div>
             </div>
         </div>
