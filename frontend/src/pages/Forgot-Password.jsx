356f5e4d5a85e1e2ae9b51c519b88185c0dc3338
Krish1212 <menikabaranwal121@gmail.com>
2025-09-05 10:26:13 +0530

    S

--- frontend/src/pages/Forgot-Password.jsx
+++ frontend/src/pages/Forgot-Password.jsx
@@ -10,22 +10,24 @@ function ForgotPassword() {
                     <IoIosArrowRoundBack size={30} className="text-[#ff6b6b]" />
                     <h1 className="text-2xl font-bold text-center text-[#ff6b6b]">Forgot Password</h1>
                 </div>
-                {step === 1 && (
-                    <div>
-                        <div className="mb-4">
-                             <label htmlFor="email" className="block text-gray-700 font-medium mb-1">Email</label>
-          <input
-            type="email"
-            id="email"
-            className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:border-orange-500"
-            placeholder="Enter your email"
-            style={{ border: `1px solid #ff6b6b` }}
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </div>
-                    </div>
-                )}
+                <div>
+                    {step === 1 && (
+                        <div>
+                            <div className="mb-4">
+                                <label htmlFor="email" className="block text-gray-700 font-medium mb-1">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:border-orange-500"
+                                    placeholder="Enter your email"
+                                    style={{ border: `1px solid #ff6b6b` }}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                />
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>  
     );
