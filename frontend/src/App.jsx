e939a45c324eb3b2ff36eae4216de4a38fd31aef
Krish1212 <menikabaranwal121@gmail.com>
2025-09-07 23:27:24 +0530

    working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { useSelector } from "react-redux";
 import Home from "./pages/Home";
 import useGetCity from "./hooks/useGetCity";
 import CreateEditShop from "./pages/CreateEditShop";
+import AddItem from "./pages/AddItem";
 export const serverUrl = "http://localhost:8000"
 
 function App(){
@@ -22,6 +23,7 @@ function App(){
       <Route path="/forgot-password" element={!userData?<ForgotPassword />: <Navigate to={"/"}/>} />
       <Route path="/" element={userData?<Home/>: <Navigate to={"/signin"}/>} />
       <Route path="/create-edit-shop" element={userData?<CreateEditShop/>: <Navigate to={"/signin"}/>} />
+      <Route path="/add-item" element={userData?<AddItem/>: <Navigate to={"/signin"}/>} />
     </Routes>
   );
 }
