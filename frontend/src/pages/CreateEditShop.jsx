8b573c5204e0d2ef3b869795582b78c987d4f766
Krish1212 <menikabaranwal121@gmail.com>
2025-09-07 22:19:50 +0530

    buildng

--- frontend/src/pages/CreateEditShop.jsx
+++ frontend/src/pages/CreateEditShop.jsx
@@ -1,18 +1,49 @@
 import React from "react";
 import { IoIosArrowRoundBack } from "react-icons/io";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { FaUtensils } from "react-icons/fa";
 import { useState } from "react";
+import { useRef } from "react";
+import axios from "axios";
+import { serverUrl } from "../App";
+import { setMyShopData } from "../redux/ownerSlice";
 
 function CreateEditShop(){
     const navigate = useNavigate()
     const {myShopData}=useSelector(state=>state.owner)
     const {currentCity, currentState,currentAddress}=useSelector(state=>state.user)
-    const {name,setName}=useState(myShopData?.name || "" )
-    const {address,setAddress}=useState(myShopData?.address|| currentAddress)
-    const {City,setCity}=useState(myShopData?.city || currentCity)
-    const {state,setState}=useState(myShopData?.city || currentState)
+    const [name,setName]=useState(myShopData?.name || "" )
+    const [address,setAddress]=useState(myShopData?.address|| currentAddress)
+    const [City,setCity]=useState(myShopData?.city || currentCity)
+    const [state,setState]=useState(myShopData?.city || currentState)
+    const [frontendImage,setFrontendImage]=useState(myShopData?.image || null)
+    const [backendImage,setBackendImage]=useState(null)
+    const dispatch=useDispatch()
+    const handleImage = (e) =>{
+        const file = e.target.files[0]
+        setBackendImage(file)
+        setFrontendImage(URL.createObjectURL(file))
+    }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        try {
+            const formData = new FormData()
+            formData.append("name",name)
+            formData.append("city",City)
+            formData.append("state",state)
+            formData.append("address",address)
+            if(backendImage){
+                formData.append("image",backendImage)
+            }
+            const result = await axios.post(`${serverUrl}/api/shop/create-edit`, formData, {withCredentials:true})
+            dispatch(setMyShopData(result.data))
+            console.log(result.data)
+        } catch (error) {   
+            console.log(error)
+        }
+    }
     return (
         <div className="flex justify-center flex-col items-center p-6 bg-gradient-to-br from-orange-50 relative to-white min-h-screen">
             <div className="absolute top-[20px] left-[20px] z-[10] mb-[10px]">
@@ -28,14 +59,19 @@ function CreateEditShop(){
                         {myShopData?"Edit Shop":"Add Shop"}
                     </div>
                 </div>
-                <form className="space-y-5">
+                <form className="space-y-5" onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="" className="block text-sm font-medium text-gray-700 mb-1">Name</label>
                         <input type="text" placeholder="Enter Shop Name" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500" onChange={(e)=>setName(e.target.value)} value={name}/>
                     </div>
                     <div>
                         <label htmlFor="" className="block text-sm font-medium text-gray-700 mb-1">Shop Image</label>
-                        <input type="file" accept="image/*" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500" />
+                        <input type="file" accept="image/*" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500" onChange={handleImage} />
+                        {frontendImage &&
+                        <div className="mt-4">
+                            <img src={frontendImage} alt=""  className="w-full h-48 object-cover rounded-lg border"/>
+                        </div>
+                        }
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div>
@@ -49,7 +85,7 @@ function CreateEditShop(){
                     </div>
                     <div>
                         <label htmlFor="" className="block text-sm font-medium text-gray-700 mb-1">Adress</label>
-                        <input type="text" placeholder="Enter Shop Adress" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500" />
+                        <input type="text" placeholder="Enter Shop Adress" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500" onChange={(e)=>setAddress(e.target.value)} value={address} />
                     </div>
                     <button className="w-full bg-[#ff4d2d] text-white px-6 py-3 rounded-lg font-semibold shadow-md hover:bg-orange-600 hover:shadow-lg transition-all duration-200 cursor-pointer">
                         Save
