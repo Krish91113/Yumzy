187d373088d8e266221c9af2aedfa74c6009c892
Krish1212 <menikabaranwal121@gmail.com>
2025-09-23 11:56:36 +0530

    Working

--- frontend/src/Components/CategoryCard.jsx
+++ frontend/src/Components/CategoryCard.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 
-function CategoryCard (){
+function CategoryCard ({data}){
     return (
-        <div>hi</div>
+        <div className="w-[120px] h-[120px] md:w-[180px] md:h-[180px] rounded-2xl border-2 border-[#ff4d2d] shrink-0 bg-white shadow-xl shadow-gray-200  overflow-hidden cursor-pointer hover:scale-105 duration-300 relative">
+            <img src={data.image} alt={data.title} className="w-full h-full object-cover" />
+            <div>
+                <h1 className="absolute bottom-0 left-0 w-full bg-[#ffffff96] bg-opacity-95 px-3 py-1 rounded-t-xl text-center shadow text-white font-medium text-lg text-gray-800 md:text-xl backdrop-blur">{data.category}</h1>
+            </div>
+        </div>
     );
 }
 export default CategoryCard;
--- frontend/src/Components/UserDashboard.jsx
+++ frontend/src/Components/UserDashboard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Nav from "./Nav";
+import { catgories } from "../category";
+import CategoryCard from "./CategoryCard";
 
 function UserDashboard(){
     return (
@@ -7,7 +9,13 @@ function UserDashboard(){
             <Nav/>
         <div className="w-full max-w-6xl flex flex-col gap-5 p-[10px] items-start">
             <h1 className="text-gray-800 text-2xl sm:text-3xl">Inspiration for your first order</h1>
-            
+            <div className="w-full">
+                <div className="w-full flex gap-4 overflow-x-auto pb-3">
+                {catgories.map((cate,index)=>{
+                    return <CategoryCard key={index} data={cate}/>
+                })}
+                </div>
+            </div>
         </div>
         </div>
     )
