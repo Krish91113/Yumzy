462bb0e4bd96911d273d5ff83e00f7fd42ee096b
Krish1212 <menikabaranwal121@gmail.com>
2025-09-23 11:31:11 +0530

    going to work

--- frontend/src/Components/UserDashboard.jsx
+++ frontend/src/Components/UserDashboard.jsx
@@ -3,7 +3,13 @@ import Nav from "./Nav";
 
 function UserDashboard(){
     return (
-        <Nav/>
+        <div className="w-screen min-h-screen flex flex-col gap-5 items-center bg-[#fff9f6] overflow-y-auto">
+            <Nav/>
+        <div className="w-full max-w-6xl flex flex-col gap-5 p-[10px] items-start">
+            <h1 className="text-gray-800 text-2xl sm:text-3xl">Inspiration for your first order</h1>
+            
+        </div>
+        </div>
     )
 }
 
