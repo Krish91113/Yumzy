431854226d13d60baef03f175ccd4ad23906c16e
Krish1212 <menikabaranwal121@gmail.com>
2025-10-03 05:27:50 +0530

    Working

--- frontend/src/Components/UserOrderCard.jsx
+++ frontend/src/Components/UserOrderCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-function userOrderCard(){
+function UserOrderCard(){
     return (
-        <div></div>
+        <div className="bg-white rounded-lg shadow p-4 space-y-4">
+
+        </div>
     )
 }
 
-export default userOrderCard
+export default UserOrderCard
