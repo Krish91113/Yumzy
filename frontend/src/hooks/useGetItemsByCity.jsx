d2062c0b6974b7fd47c3e037449b64cd2dce351f
Krish1212 <menikabaranwal121@gmail.com>
2025-10-02 05:41:19 +0530

    Working

---
+++ frontend/src/hooks/useGetItemsByCity.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { serverUrl } from "../App";
+import { useDispatch, useSelector } from "react-redux";
+import { setShopsInMyCity } from "../redux/userSlice";
+
+function useGetItemsByCity() {
+  const dispatch = useDispatch();
+  const { currentCity } = useSelector(state => state.user);
+
+  useEffect(() => {
+    if (!currentCity) return; // wait until currentCity is set
+
+    const fetchShops = async () => {
+      try {
+        const result = await axios.get(
+          `${serverUrl}/api/shop/get-by-city/${currentCity}`,
+          { withCredentials: true }
+        );
+        dispatch(setShopsInMyCity(result.data));
+        console.log("Shops fetched:", result.data);
+      } catch (error) {
+        console.error("Error fetching shops by city:", error.response?.data || error.message);
+        dispatch(setShopsInMyCity([])); // reset to empty on error
+      }
+    };
+
+    fetchShops();
+  }, [currentCity, dispatch]);
+}
+
+export default useGetItemsByCity;
