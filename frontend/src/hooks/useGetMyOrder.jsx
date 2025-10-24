684a91f6cb73358f9f170d1eb510fafb2d9feb15
Krish1212 <menikabaranwal121@gmail.com>
2025-10-24 18:18:57 +0530

    Solving errors

--- frontend/src/hooks/useGetMyOrder.jsx
+++ frontend/src/hooks/useGetMyOrder.jsx
@@ -1,32 +1,33 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setItemsInMyCity } from "../redux/userSlice";
+import {  setMyOrders } from "../redux/userSlice";
 import { serverUrl } from "../App";
 
-function useGetItemsByCity() {
+function useGetMyOrders() {
   const dispatch = useDispatch();
   const { currentCity } = useSelector(state => state.user);
-
+  const { userData } = useSelector(state => state.user);
   useEffect(() => {
+    
     if (!currentCity) return;
-
-    const fetchItems = async () => {
+    
+    const fetchOrders = async () => {
       try {
         const result = await axios.get(
-          `${serverUrl}/api/item/get-by-city/${currentCity}`,
+          `${serverUrl}/api/order/my-orders`,
           { withCredentials: true }
         );
-        dispatch(setItemsInMyCity(result.data));
-        console.log("Items fetched:", result.data);
+        dispatch(setMyOrders(result.data));
+        console.log("Orders fetched:", result.data);
       } catch (error) {
-        console.error("Error fetching items:", error.response?.data || error.message);
-        dispatch(setItemsInMyCity([]));
+        console.error("Error fetching orders:", error.response?.data || error.message);
+        dispatch(setMyOrders([]));
       }
     };
 
-    fetchItems();
-  }, [currentCity, dispatch]);
+    fetchOrders();
+  }, [userData]);
 }
 
-export default useGetItemsByCity;
+export default useGetMyOrders;
