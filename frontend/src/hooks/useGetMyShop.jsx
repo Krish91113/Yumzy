e39f148709be82d0094d7d5e7fd344328d5c77fe
Krish1212 <menikabaranwal121@gmail.com>
2025-10-24 12:22:44 +0530

    Solving errors

--- frontend/src/hooks/useGetMyShop.jsx
+++ frontend/src/hooks/useGetMyShop.jsx
@@ -1,19 +1,20 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { use, useEffect } from "react";
 import { serverUrl } from "../App";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setMyShopData } from "../redux/ownerSlice";
 
 function useGetMyShop(){
     const dispatch=useDispatch()
-    useEffect(() =>{
+     const {userData}=useSelector(state=>state.user)
+    useEffect(() => {
         const fetchShop = async () =>{
             try{
 
                 const result =await axios.get(`${serverUrl}/api/shop/get-my`, {withCredentials:true})
                 dispatch(setMyShopData(result.data))
             }catch(error){
-
+                console.log(error)
             }
         }
         fetchShop()
